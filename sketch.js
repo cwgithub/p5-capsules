a5b3eb0e76dfbdaa264b1eae4e07665a740c14f1
Chris Ward <cw@yellowdave.co.uk>
2025-03-11 19:49:20 +0000

    extended

--- sketch.js
+++ sketch.js
@@ -14,35 +14,43 @@ function setup() {
   // seed - 81790;
   // 15968
   // 23135
+  // 89674
+  // 47239
+  // 55587
+  // 67031
 
   // Set the random seed
   randomSeed(seed);
   noiseSeed(seed);
+  angleMode(DEGREES);
 
   console.log("Random Seed:", seed);
 
   // createCanvas(2481, 3507);
   createCanvas(2500, 2500);
 
-  angleMode(DEGREES);
-
   colorMode(HSB, 360, 100, 100, 100); // Use HSB for easy manipulation
+
   baseColor = color(30, 100, 100); // Approximate HSB of (255,128,0) in RGB
 
-  for (let i = 0; i < 5; i++) {
-    let hueShift = (hue(baseColor) + random(-10, 10)) % 360;
-    let satShift = constrain(saturation(baseColor) + random(-30, 30), 0, 100);
-    let brightShift = constrain(
-      brightness(baseColor) + random(-30, 30),
-      0,
-      100
-    );
-    let alphaShift = random(50, 100);
-    variations.push(color(hueShift, satShift, brightShift, alphaShift));
-  }
+  setupColourVariations(baseColor);
 
   background(255);
   noLoop();
+}
+
+function setupColourVariations(baseColor) {
+  for (let i = 0; i < 5; i++) {
+    let hueShift = (hue(baseColor) + random(-50, 50)) % 360;
+    let satShift = constrain(saturation(baseColor) + random(-30, 30), 0, 100);
+    let brightShift = constrain(
+      brightness(baseColor) + random(-50, 50),
+      0,
+      100
+    );
+    let alphaShift = random(10, 100);
+    variations.push(color(hueShift, satShift, brightShift, alphaShift));
+  }
 }
 
 function draw() {
@@ -60,19 +68,15 @@ function draw() {
   console.log(pixelDensity());
   imageMode(CENTER);
 
-  for (let c = 0; c < 25; c++) {
+  for (let c = 0; c < 10; c++) {
     push();
-
     const capLength = random(200, 400);
-    const capFat = random(20, 100);
-
+    const capFat = random(20, 150);
     const c = capsule(capLength, capFat, variations[0]);
-
-    x = random(150, 2000);
-    y = random(150, 2000);
-
+    x = random(250, 2000);
+    y = random(150, 2400);
     translate(x, y);
-    // rotate(45);
+    rotate(45);
     image(c.img, 0, 0);
     pop();
   }
@@ -81,10 +85,23 @@ function draw() {
     push();
     const capLength = random(200, 400);
     const capFat = random(20, 100);
-    const c = capsule(capLength, capFat, variations[3]);
+    const c = capsule(capLength, capFat, variations[1]);
     x = random(200, 2000);
     y = random(200, 2000);
     translate(x, y);
+    rotate(45);
+    image(c.img, 0, 0);
+    pop();
+  }
+
+  for (let c = 0; c < 3; c++) {
+    push();
+    const capLength = random(400, 800);
+    const capFat = random(20, 100);
+    const c = capsule(capLength, capFat, variations[3]);
+    x = random(200, 2000);
+    y = random(2000, 2200);
+    translate(x, y);
     // rotate(45);
     image(c.img, 0, 0);
     pop();
@@ -92,15 +109,11 @@ function draw() {
 
   for (let c = 0; c < 5; c++) {
     push();
-
     const capLength = random(400, 800);
     const capFat = random(20, 100);
-
-    const c = capsule(capLength, capFat, variations[3]);
-
-    x = random(200, 2000);
-    y = random(2000, 2200);
-
+    const c = capsule(capLength, capFat, variations[4]);
+    x = 2000;
+    y = random(1, 50) * 100;
     translate(x, y);
     // rotate(45);
     image(c.img, 0, 0);
@@ -143,6 +156,14 @@ function createFrame(frameWidth, frameHeight) {
   frameGraphics.push();
   frameGraphics.fill(variations[2]);
   frameGraphics.rect(frameWidth / 2, frameHeight / 2, frameWidth, frameHeight);
+
+  for (var i = 0; i < frameHeight; i += 10) {
+    frameGraphics.fill(variations[4]);
+
+    frameGraphics.rect(frameWidth / 2, i, frameWidth, random(3, 7));
+  }
+
+  frameGraphics.filter(BLUR, 5);
   frameGraphics.pop();
 
   return frameGraphics;
